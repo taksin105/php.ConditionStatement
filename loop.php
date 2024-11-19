fff07c36a53e5f486c51891b85b47fc5dd2a9713
taksin105 <66040233105@udru.ac.th>
2024-11-19 11:39:57 +0700

    update 19/11/67

---
+++ loop.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>คำสั่ง Loop ใน php</title>
+</head>
+<body>
+    <h1>การใช้คำสั่งแบบวนรอบ Loop Statement</h1>
+    
+    <?php
+        echo "<h2>while loop</h2>";
+        echo "<br>";
+
+        $x = 1;
+        while ($x <= 5) { if ($x==3) break;
+            echo "รอบที่ $x<br>";
+        $x++;
+        
+
+        }
+        $i = 1;
+        while ($i <= 40) {echo "-"; $i++;}
+        echo "<br>";
+
+        echo "<h2>do while loop</h2>";
+        echo "<br>";
+
+        $y = 1;
+        do {
+            echo "do while รอบที่ $y <br>";
+            $y++;
+            if($y >= 5) break;
+        } while ($y <= 5);
+        echo "-----------------------------------------------<br>";
+
+        echo "<h2>For loop</h2>";
+        for ($i=0; $i < 10; $i++) { 
+            echo "For รอบที่ $i<br>";
+        }
+        echo "-----------------------------------------------<br>";
+    
+        echo "<h2>For loop ซ้อนกัน</h2>";
+        for ($i = 1; $i <= 5; $i++) { 
+            for ($j = 1; $j <= 5; $j++) {
+                echo "*";
+            }
+            echo "<br>";
+        }
+        echo "-----------------------------------------------<br>";
+
+        echo "<h2>Foreach loop แบบปกติ</h2>";
+        $pet = array("แมว","สุนัข","นกแก้ว","ปลาทอง","หมูเด้ง");
+        foreach ($pet as $value) {
+            echo "ฉันชอบ" .$value."<br>";
+        }
+        echo "-----------------------------------------------<br>";
+        
+        echo "<h2>Foreach loop แบบ key=>value </h2>";
+        $food = array("หมวย"=> "ส้มตำ","บูม" =>"หมูย่าง","บีม"=>"ผัดกระเพรา","อาร์ต"=>"ข้าวมันไก่");
+        foreach ($food as $student => $value) {
+            echo "ชื่อ $student ชอบกิน $value<br>";
+        }
+    ?>
+
+
+
+
+</body>
+</html>
